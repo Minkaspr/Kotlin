e556854f3cb104372ef0478b500009813a8b99e3
Dario Quispe <rubendarioqa@hotmail.com>
2023-05-19 16:06:57 -0500

    Se agrega el tema 3

--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -8,7 +8,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        variablesYConstantes()
+        // variablesYConstantes()
+        tiposDeDatos()
 
     }
 
@@ -81,4 +82,70 @@ class MainActivity : AppCompatActivity() {
         println(mySecondConstant) // -> ¿Qué haces?
 
     }
+
+
+    private fun tiposDeDatos() {
+
+        /**
+         * 3ro - Tipos de Datos
+         */
+
+
+        /*
+         STRING
+        */
+
+        // Android Studio emite una advertencia porque kotlin infiere el tipo de dato
+        val myString1: String = "Hola Developer"
+        // Kotlin deduce automaticamente que es de tipo String
+        val myString2 = "Haz vuelto"
+
+        // Concatenar
+        val myString3 = myString1 + ", " +myString2
+        // Mostrar por consola
+        println(myString3) // -> Hola Developer, Haz vuelto
+
+
+        /*
+         Enteros (Byte, Short, Int, Long)
+        */
+
+        val myInt1: Int = 1
+        val myInt2 = 2
+        val myInt3 = myInt1 + myInt2
+        println(myInt3) // -> 3
+
+
+        /*
+         Decimales (Float, Double)
+        */
+
+        val myFloat: Float = 1.5f
+        val myDouble1: Double = 1.5
+        val myDouble2 = 2.6
+        val myDouble3 = 1 // <>  1.0 pero en realidad es un Int
+        val myDouble4 = myDouble1 + myDouble2 + myDouble3
+        println(myDouble4) // -> 5.1
+
+
+        /*
+         Boolean (Bool)
+        */
+
+        val myBool1: Boolean = true
+        val myBool2 = false
+
+        /*
+        *  val myBool3 = myBool1 + myBoll2 // -> Error
+        *
+        * Error:
+        *   No se puede efectuar en algunas operaciones
+        *   solo en operaciones logicas
+        * */
+
+        println(myBool1 == myBool2) // -> false
+        println(myBool1 && myBool2) // -> false
+
+    }
+
 }
