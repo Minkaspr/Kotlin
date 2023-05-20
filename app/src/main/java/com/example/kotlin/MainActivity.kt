8568f74ca03df0111185298ec885c9c8f55ffefa
Dario Quispe <rubendarioqa@hotmail.com>
2023-05-20 13:12:07 -0500

    Se agrega el tema 4, 5 y 6

--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -9,7 +9,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // variablesYConstantes()
-        tiposDeDatos()
+
+        // tiposDeDatos()
+
+        sentenciaIf()
 
     }
 
@@ -87,7 +90,7 @@ class MainActivity : AppCompatActivity() {
     private fun tiposDeDatos() {
 
         /**
-         * 3ro - Tipos de Datos
+         * 3ro - TIPOS DE DATOS
          */
 
 
@@ -146,6 +149,114 @@ class MainActivity : AppCompatActivity() {
         println(myBool1 == myBool2) // -> false
         println(myBool1 && myBool2) // -> false
 
+    }
+
+
+    private fun sentenciaIf(){
+
+        /**
+         *  4to - SENTENCIA If
+         *
+         *  Sirve para evaluar condicionoes
+         */
+
+        val myNumber = 10
+        // Para mostrar el numero usamos el '$' antes del nombre de la variable o constante
+        println("$myNumber es menor que 10") // -> 10 es menor que 10
+
+        /*
+        * Operadores condicionales
+        *
+        *   >     mayor que
+        *   <     menor que
+        *   >=    mayor o igual que
+        *   <=    menor o igual que
+        *   ==    igual que
+        *   !=    desigual que
+        *
+        * */
+
+        // Evaluamos la condicion, si myNumber es menor que 10
+        // Luego mostramos por consola solo si se cumple la condicion
+
+        if (myNumber < 10) {
+            // 10 no es menor 10, por ende no se muestra nada por consola
+            println("$myNumber es menor que 10")
+        }
+
+        var myNumber2 = 4
+        if (myNumber2 < 10) {
+            println("$myNumber es menor que 10") // -> 4 es menor que 10
+        }
+
+
+        /**
+         *  5to - SENTENCIA ELSE
+         *
+         *  Se puede utilizar solo si anteriormente se usa la sentencia if
+         *  y Sirve para designar un bloque de codigo, y se ejecuta solo
+         *  cuando no se ejecute la sentencia if
+         */
+
+        myNumber2 = 12
+        if (myNumber2 <=10) {
+            // Sentencia if
+            println("$myNumber2 es menor o igual a 10")
+        } else {
+            // Sentencia else
+            println("$myNumber2 es mayor a 10") // -> 12 es mayor a 10
+        }
+
+
+        /*
+        * Operadores Logicos
+        *
+        *   &&  "y"     Todas las condiciones deben ser correctas
+        *   ||  "o"     Una de las condiciones debe ser correcta
+        *   !   "no"    Se niega la condicion
+        *
+        * */
+
+        myNumber2 = 7
+        if (myNumber2 <=10 && myNumber2 > 5) {
+            println("$myNumber2 es menor o igual a 10 y mayor a 5") // -> 7 es menor o igual a 10 y mayor a 5
+        } else {
+            // Solo se ejecuta el else cuando es opuesto a la condicion del if
+            println("$myNumber2 es mayor a 10 o menor o igual a 5")
+        }
+
+
+        myNumber2 = 53
+        if ((myNumber2 <=10 && myNumber2 > 5) || myNumber2 == 53) {
+            // <- Se muestra esto por consola ->
+            println("$myNumber2 es menor o igual a 10 y mayor a 5 o es igual a 53")
+        } else {
+            println("$myNumber2 es mayor a 10 o menor o igual a 5 y no es igual a 53")
+        }
+
+
+        /**
+         *  6to - SENTENCIA ELSE IF
+         *
+         *  Se usa entre un if y else
+         *  Funciona como un nuevo if, donde se pone una condicion
+         */
+
+        myNumber2 = 60
+        if ((myNumber2 <=10 && myNumber2 > 5) || myNumber2 == 53) {
+            println("$myNumber2 es menor o igual a 10 y mayor a 5 o es igual a 53")
+        } else if (myNumber2 != 60) {
+            // Sentencia else if
+            println("$myNumber2 no es igual a 60")
+        } else if (!(myNumber2 == 60)) {
+            // Sentencia else if
+            println("$myNumber2 no es igual a 60")
+        } else if (myNumber2 == 60) {
+            // Sentencia else if
+            println("$myNumber2 es igual a 60") // -> 60 es igual a 60
+        } else {
+            println("$myNumber2 es mayor a 10 o menor o igual a 5 y no es igual a 53")
+        }
     }
 
 }
